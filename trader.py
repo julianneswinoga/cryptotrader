80cc939cc8c8ba10488e4ed1d503bb670d1e10aa
Julianne Swinoga <julianneswinoga@gmail.com>
2017-10-18 16:51:39 -0400

    Initial database structure set

--- trader.py
+++ trader.py
@@ -1,17 +1,52 @@
 #!/usr/bin/python
 
-import bittrex as b
+from bittrex import Bittrex
+import json, sqlite3
 
 class Wallet(object):
     def __init__(self, coin_ticker, initial_value):
         self.ticker = coin_ticker
         self.value = initial_value
 
+
+class Market(object):
+    def __init__(self, db_cursor, bittrex_connection, market_string):
+        self.market_string = market_string
+        self.table_name = market_string.replace('-', '_')
+        self.db = db_cursor
+        self.bit = bittrex_connection
+
+        self.db.execute("CREATE TABLE IF NOT EXISTS %s (Id INT PRIMARY KEY, TimeStamp DATE, Total FLOAT, Price FLOAT, Quantity FLOAT, OrderType TEXT, FillType TEXT)" % self.table_name)
+
+    def update(self):
+        market_result = self.bit.get_market_history(self.market_string)['result']
+        if len(market_result) > 0:
+            keys = market_result[0].keys()
+            db_statement = "INSERT INTO %s (%s) VALUES(%s)" % (self.table_name, ','.join(keys), ','.join([':%s' % s for s in keys]))
+            self.db.executemany(db_statement, market_result)
+
+    def get_data(self):
+        self.db.execute("SELECT * FROM %s" % self.table_name)
+        rows = self.db.fetchall()
+        for row in rows:
+            print row
+
+
 if __name__ == '__main__':
-    my_bittrex = b.Bittrex(None, None, api_version='v1.1')
-    print my_bittrex.get_markets()
+    my_bittrex = Bittrex(api_key=None, api_secret=None)
+    dbconn = sqlite3.connect('market.db')
+    db = dbconn.cursor()
 
-    eth_wallet = Wallet('ETH', 0.4)
-    zec_wallet = Wallet('ZEC', 0.5)
+    eth_usd_market = Market(db, my_bittrex, 'USDT-ETH')
+    eth_usd_market.update()
 
-    print 'Hello!'
+    eth_usd_market.get_data()
+
+    # market_result = my_bittrex.get_market_history('USDT-ETH')['result']
+    # print len(market_result)
+    # print market_result[0]['TimeStamp']
+    # print market_result[-1]['TimeStamp']
+    # print market_result[0]
+
+    # eth_wallet = Wallet('ETH', 0.4)
+    # zec_wallet = Wallet('ZEC', 0.5)
